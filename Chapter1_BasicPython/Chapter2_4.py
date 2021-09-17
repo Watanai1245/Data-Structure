29305e1a39b647866cab406719c5fa54bfe3d3cc
Watanai <69137758+Watanai1245@users.noreply.github.com>
2021-09-17 23:32:53 +0700

    Delete Chapter1_BasicPython directory

--- Chapter1_BasicPython/Chapter2_4.py
+++
@@ -1,7 +0,0 @@
-def hbd(age):
-    if(age%2 == 0):
-        return "saimai is just {}, in base {}!".format(20,age//2)
-    else:
-        return "saimai is just {}, in base {}!".format(21,age//2)
-year = input("Enter year : ")
-print(hbd(int(year)))
