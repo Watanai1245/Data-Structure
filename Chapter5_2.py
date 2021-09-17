7577d553ec8386418f0dcb8dd47f265ac8c0f5b2
Watanai1245 <new0820571245@gmail.com>
2021-09-17 15:21:09 +0700

    Edit

--- Chapter5_2.py
+++ Chapter5_2.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, value):
+        self.value = value
         self.next = None
         self.previous = None
 
@@ -8,145 +8,180 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self._size = 0
+        self.size = 0
 
     def __str__(self):
         if self.isEmpty():
-            return ''
-        else:
-            current, word = self.head, str(self.head.data)
-            while current.next != None:
-                word += '->' + str(current.next.data)
-                current = current.next
-            return word
+            return "Empty"
+        cur, s = self.head, str(self.head.value) + " "
+        while cur.next != None:
+            s += str(cur.next.value) + " "
+            cur = cur.next
+        return s
 
-    def str_reverse(self):
+    def reverse(self):
         if self.isEmpty():
-            return ''
-        else:
-            current, word = self.tail, str(self.head.data)
-            while current.previous != None:
-                word += '->' + str(current.previous.data)
-                current = current.previous
-            return word
+            return "Empty"
+        cur, s = self.tail, str(self.tail.value) + " "
+        while cur.previous != None:
+            s += str(cur.previous.value) + " "
+            cur = cur.previous
+        return s
 
     def isEmpty(self):
-        return self.head == None
+        return self.size == 0
 
     def append(self, item):
         if self.isEmpty():
             self.head = self.tail = Node(item)
-            self._size += 1
-            return 0
-        else:
-            current = self.head
-            while(current.next != None):
-                current = current.next
-            node = Node(item)
-            node.previous = current
-            current.next = node
-            self.tail = node
-            self._size += 1
+            self.size += 1
+            return
 
-    def add_before(self,item):
-        if self.isEmpty():
-            self.head = self.tail = Node(item)
-            self._size += 1
-            return 0
-        else:
-            node = Node(item)
-            if self.size() == 1:
-                node.next = self.tail
-                self.tail.previous = node
-                self.head = node
-            else:
-                node.next = self.head
-                self.head.previous = node
-                self.head = node
-            self._size += 1
+        new_tail = Node(item)
+        old_tail = self.tail
 
-    def insert(self,pos,item):
-        if pos > self.size() or pos <0:
-            return print('Data cannot be added')
-
-        if self.isEmpty():
-            self.head = self.tail = Node(item)
-            self._size += 1
-            return print(f'index = {pos} and data = {item}')
-        node = Node(item)
-
-        if pos == 0:
-            self._size +=1
-            node.next = self.head
-            self.head.previous = node
-            self.head = node
-
-        if pos == self.size():
-            self._size += 1
-            node.previous = self.tail
-            self.tail.next = node
-            self.tail = node
-        current , index = self.head , 0
-
-        while index != pos -1:
-            index += 1
-            current = current.next
+        old_tail.next = new_tail
+        new_tail.previous = old_tail
         
-        current.next.previous = node
-        node.next = current.next
-        node.previous = current
-        current.next = node
-        self._size += 1
-        return print(f'index = {pos} and data = {item}')
+        self.tail = new_tail
+        self.size += 1
+
+    def addHead(self, item):
+        if self.isEmpty():
+            self.head = self.tail = Node(item)
+            self.size += 1
+            return
+        
+        new_head = Node(item)
+        old_head = self.head
+
+        new_head.next = old_head
+        old_head.previous = new_head
+
+        self.head = new_head
+        self.size += 1
+
+    def insert(self, pos, item):
+
+        inserted_node = Node(item)
+        if pos == 0:
+            self.addHead(item)
+            return
+        if pos > 0:
+            if pos >= self.size - 1:
+                self.append(item)
+                return
+            else:
+                node = self.head
+                for _ in range(0,pos):
+                    node = node.next
+
+                previous_node = node
+                next_node = previous_node.next
+
+                previous_node.next = inserted_node
+                inserted_node.previous = previous_node
+
+                next_node.previous = inserted_node
+                inserted_node.next = next_node
+        else:
+            if pos <= -self.size:
+                self.addHead(item)
+                return
+            else:
+                node = self.tail
+                for _ in range(-1, pos-1, -1):
+                    node = node.previous
+
+                previous_node = node
+                next_node = previous_node.next
+
+                previous_node.next = inserted_node
+                inserted_node.previous = previous_node
+
+                next_node.previous = inserted_node
+                inserted_node.next = next_node
+
+        self.size += 1
+
+    def search(self, item):
+        node = self.head
+        while node is not None:
+            if node.value == item:
+                return "Found"
+            node = node.next
+        return "Not Found"
+
+    def index(self, item):
+        index = 0
+        node = self.head
+        while node is not None:
+            if node.value == item:
+                return index
+            node = node.next
+            index += 1
+        return -1
 
     def size(self):
-        return self._size
+        return self.size
 
-    def remove(self,item):
-        if self.isEmpty():
-            return print("Not Found!")
-        current = self.head
+    def pop(self, pos):
+        if self.size-1 < pos or pos < 0:
+            return "Out of Range"
 
-        if self.size() == 1 and current.data == item:
-            self.head = self.tail = None
-            self._size -= 1
-            return print(f'removed : {item} from index : 0')
-        self._size -= 1
+        if self.size == 1:
+            self.head = None
+            self.tail = None
 
-        if item == self.head.data:
-            self.head = self.head.next
-            self.head.previous = None
-            return print(f'removed : {item} from index : 0')
-        index = 0
+        elif pos == 0:
+            print(f"l {self}")
+            print("size "+ str(self.size))
+            new_head = self.head.next
+            new_head.previous = None
 
-        while current.next != None:
-            if current.data == item:
-                current.previous.next = current.next
-                current.next.previous = current.previous
-                return print(f'removed : {item} from index : {index}')
-            index += 1
-            current = current.next
-        if current.data == item:
-            self.tail = self.tail.previous
-            self.tail.next = None
-            return print(f'removed : {item} from index : {index}')
-        self._size += 1
-        return print('Not Found!')
+            self.head = new_head
+            
+        elif pos == self.size-1:
+            new_tail = self.tail.previous
+            new_tail.next = None
 
-if __name__ == '__main__':
-    Inp = input('Enter Input : ').split(',')
+            self.tail = new_tail
 
-    L = LinkedList()
+        else:
+            node = self.head
+            for _ in range(0, pos):
+                node = node.next
 
-    for a in Inp:
-        a = a.split()
-        if a[0] == 'A':
-            L.append(a[1])
-        elif a[0] == 'Ab':
-            L.add_before(a[1])
-        elif a[0] == 'I':
-            L.insert(int(a[1].split(':')[0]),a[1].split(':')[1])
-        elif a[0] == 'R':
-            L.remove(a[1])
-        print('linked list :',L)
-        print('reverse :',L.str_reverse())
+            previous_node = node.previous
+            next_node = node.next
+
+            if previous_node is not None:
+                previous_node.next = next_node
+                next_node.previous = previous_node
+
+        self.size -= 1
+
+        return "Success"
+
+L = LinkedList()
+inp = input('Enter Input : ').split(',')
+for i in inp:
+    if i[:2] == "AP":
+        L.append(i[3:])
+    elif i[:2] == "AH":
+        L.addHead(i[3:])
+    elif i[:2] == "SE":
+        print("{0} {1} in {2}".format(L.search(i[3:]), i[3:], L))
+    elif i[:2] == "SI":
+        print("Linked List size = {0} : {1}".format(L.size, L))
+    elif i[:2] == "ID":
+        print("Index ({0}) = {1} : {2}".format(i[3:], L.index(i[3:]), L))
+    elif i[:2] == "PO":
+        before = "{}".format(L)
+        k = L.pop(int(i[3:]))
+        print(("{0} | {1}-> {2}".format(k, before, L)) if k == "Success" else ("{0} | {1}".format(k, L)))
+    elif i[:2] == "IS":
+        data = i[3:].split()
+        L.insert(int(data[0]), data[1])
+print("Linked List :", L)
+print("Linked List Reverse :", L.reverse())
